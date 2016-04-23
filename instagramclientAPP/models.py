a57339af6a191c8716dca298207348f553831fb1
admin <meltemdzdr@gmail.com>
2016-04-23 14:56:31 +0300

    post yazıl db olusturuldu

--- instagramclientAPP/models.py
+++ instagramclientAPP/models.py
@@ -6,3 +6,15 @@ class Post(models.Model):
     date=models.DateTimeField(
         default=timezone.now)
     hastag=models.CharField(max_length=50)
+
+
+    def save(self, *args, **kwargs):
+       self.date = unidecode(self.date)
+       super(Post, self).save(*args, **kwargs)
+
+       self.hastag = unidecode(self.hastag)
+       super(Post, self).save(*args, **kwargs)
+
+  # def search(self):
+        #self. date = timezone.now()
+        #self.save()
