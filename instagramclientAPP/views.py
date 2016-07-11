acd62241f0ff373ef1879795193480df405d2dd7
admin <meltemdzdr@gmail.com>
2016-07-12 01:37:08 +0300

    Site için HTML dosyasını post_list.php ile değiştirdim.

--- instagramclientAPP/views.py
+++ instagramclientAPP/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    return render(request, 'instagramclientAPP/post_list.html', {})
+    return render(request, 'instagramclientAPP/post_list.php', {})
 
 # Create your views here.
